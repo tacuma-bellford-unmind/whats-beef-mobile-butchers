fba6988e4efd249cb3cad55b2d0bb001ca89055b
tacuma.bellford <tacuma.bellford@unmind.com>
2023-05-23 15:43:46 +0100

    Deleted get selection file

--- src/db/getMeatSelection.js
+++
@@ -1,7 +0,0 @@
-import * as data from "./butchers.json";
-
-export const getMeatSelection = () => {
-  const { butchers } = data;
-  const meatSelection = butchers.map((butcher) => butcher.meats);
-  return { meatSelection };
-};
